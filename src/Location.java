bcc8c0917f386a744616a4fc4e23370595de4296
grottI0 <chernega.bk@gmail.com>
2022-10-27 22:13:39 +0300

    first

---
+++ src/Location.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Type;
+import java.util.Objects;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Location other = (Location) obj;
+
+        return this.xCoord == other.xCoord && this.yCoord == other.yCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
+}
